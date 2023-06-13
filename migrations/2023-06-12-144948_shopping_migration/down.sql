bddf97fa33a8f013980b73772797374de399ae94
Magicalyida <2939066244@qq.com>
2023-06-13 23:38:20 +0800

    new

--- migrations/2023-06-12-144948_shopping_migration/down.sql
+++ migrations/2023-06-12-144948_shopping_migration/down.sql
@@ -4,11 +4,13 @@ drop table public.shopping_cart;
 
 drop table public.orders;
 
-drop table public.users;
+drop table public.stores_products_unit;
 
 drop table public.products;
 
-drop table public.stores_products_unit;
+drop table public.user_stores_unit;
+
+drop table public.users;
 
 drop table public.stores;
 
