83d12f51df5bdf97d8a10dc882a69a730c14f1b2
Magicalyida <2939066244@qq.com>
2023-06-14 17:56:55 +0800

    no check

--- migrations/2023-06-12-144948_shopping_migration/down.sql
+++ migrations/2023-06-12-144948_shopping_migration/down.sql
@@ -1,5 +1,3 @@
-drop extension btree_gist;
-
 drop table public.orders;
 
 drop table public.shopping_carts;
@@ -9,4 +7,6 @@ drop table public.products;
 drop table public.stores;
 
 drop table public.users;
+
+drop extension btree_gist;
 
