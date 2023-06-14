c66b7be09be74b5d81b06e686ee90f8164351ffd
hercule karuha <code>
2023-06-14 14:34:41 +0800

    fix env no present

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use diesel::insert_into;
 use diesel::prelude::*;
 use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::PgConnection;
+use dotenvy::dotenv;
 use serde_json::{json, Value};
 use std::env;
 pub mod schema;
@@ -27,7 +28,9 @@ async fn main() {
 }
 
 pub fn create_db_pool() -> Pool<ConnectionManager<PgConnection>> {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL not set");
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     Pool::builder()
         .build(manager)
