f1c67656ce092ea7c5477e38ce6dc4f6119f9420
hercule karuha <code>
2023-06-14 16:18:08 +0800

    move to handlers

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 use axum::{
-    extract::{Json, State},
-    response,
     routing::post,
     Router, Server,
 };
-use axum_macros::debug_handler;
-use diesel::insert_into;
-use diesel::prelude::*;
 use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::PgConnection;
 use dotenvy::dotenv;
-use serde_json::{json, Value};
 use std::env;
+use handlers::*;
+
 pub mod schema;
+pub mod handlers;
 
 #[tokio::main]
 async fn main() {
@@ -37,49 +34,4 @@ pub fn create_db_pool() -> Pool<ConnectionManager<PgConnection>> {
         .expect("Failed to create database pool")
 }
 
-#[debug_handler]
-async fn register(
-    State(pool): State<Pool<ConnectionManager<PgConnection>>>,
-    Json(payload): Json<Value>,
-) -> response::Json<Value> {
-    use self::schema::users::dsl::*;
-    let conn = &mut pool.get().unwrap();
-    let user_accont = payload.get("userName").and_then(Value::as_str).unwrap();
-    let user_password = payload.get("password").and_then(Value::as_str).unwrap();
-    let sex = payload.get("sex").and_then(Value::as_str);
-    let user_gender = match sex.unwrap() {
-        "1" => "男".to_string(),
-        "2" => "女".to_string(),
-        _ => String::new(),
-    };
-    let id = insert_into(users)
-        .values((
-            account.eq(user_accont),
-            password.eq(user_password),
-            gender.eq(user_gender),
-        ))
-        .returning(user_id)
-        .execute(conn);
-    match id {
-        Ok(inserted_id) => {
-            // 插入成功
-            response::Json(json!({
-                "code": 200,
-                "msg": "注册成功",
-                "data": {
-                    "userId": inserted_id.to_string()
-                }
-            }))
-        }
-        Err(error) => {
-            // 插入失败
-            response::Json(json!({
-                "code": 500,
-                "msg": "注册失败",
-                "data": {
-                    "userId": error.to_string()
-                }
-            }))
-        }
-    }
-}
+
