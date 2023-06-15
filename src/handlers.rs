005a7d83b28004c5d3f89837f3ca5bcfd5c93fc7
hercule karuha <code>
2023-06-15 12:32:19 +0800

    update register

--- src/handlers.rs
+++ src/handlers.rs
@@ -2,13 +2,12 @@ use axum::{
     extract::{Json, State},
     response,
 };
-use serde_json::{json, Value};
-use diesel::r2d2::{ConnectionManager, Pool};
-use diesel::PgConnection;
 use axum_macros::debug_handler;
 use diesel::insert_into;
 use diesel::prelude::*;
-
+use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::PgConnection;
+use serde_json::{json, Value};
 
 #[debug_handler]
 pub async fn register(
@@ -16,18 +15,25 @@ pub async fn register(
     Json(payload): Json<Value>,
 ) -> response::Json<Value> {
     use crate::schema::users::dsl::*;
-    let conn = &mut pool.get().unwrap();
-    let user_accont = payload.get("userName").and_then(Value::as_str).unwrap();
-    let user_password = payload.get("password").and_then(Value::as_str).unwrap();
+    let user_password = payload["password"].as_str().unwrap();
+    if user_password.len() < 8 || user_password.len() > 20 {
+        return response::Json(json!({
+            "code": 500,
+            "msg": "密码必须在8~20位之间"
+        }));
+    }
+    let usr_name = payload.get("userName").and_then(Value::as_str).unwrap();
     let sex = payload.get("sex").and_then(Value::as_str);
     let user_gender = match sex.unwrap() {
-        "1" => "男".to_string(),
-        "2" => "女".to_string(),
-        _ => String::new(),
+        "1" => 1,
+        "2" => 2,
+        _ => 0,
     };
+    let conn = &mut pool.get().unwrap();
+
     let id = insert_into(users)
         .values((
-            account.eq(user_accont),
+            user_name.eq(usr_name),
             password.eq(user_password),
             gender.eq(user_gender),
         ))
@@ -44,14 +50,11 @@ pub async fn register(
                 }
             }))
         }
-        Err(error) => {
+        Err(_) => {
             // 插入失败
             response::Json(json!({
                 "code": 500,
-                "msg": "注册失败",
-                "data": {
-                    "userId": error.to_string()
-                }
+                "msg": "用户名不唯一"
             }))
         }
     }
