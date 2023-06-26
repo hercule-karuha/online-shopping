6ea10c9135b139f6c15d18ce149d1681ca490e9f
hercule karuha <code>
2023-06-26 14:06:28 +0800

    add name in order

--- migrations/2023-06-26-060525_add_name_in_order/down.sql
+++ migrations/2023-06-26-060525_add_name_in_order/down.sql
@@ -1,1 +1,3 @@
 -- This file should undo anything in `up.sql`
+alter table orders
+    drop column if exists product_name;
--- migrations/2023-06-26-060525_add_name_in_order/up.sql
+++ migrations/2023-06-26-060525_add_name_in_order/up.sql
@@ -1,1 +1,3 @@
 -- Your SQL goes here
+alter table orders
+    add product_name varchar(100);
